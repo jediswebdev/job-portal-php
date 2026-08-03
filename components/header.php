9d644bec4cb9de3f5c72609ad1ef799b53551f6a
JedisWebDev <jediswebdev@gmail.com>
2026-08-03 12:15:27 +0100

    adding other pages

--- components/header.php
+++ components/header.php
@@ -29,20 +29,17 @@
 
       <ul
         class="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-xl bg-indigo-700 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-indigo-700 text-center">
+
         <li>
-          <a href="#" class="block py-2 px-3 text-white rounded hover:bg-gray-500"
-            aria-current="page">Home</a>
-        </li>
-        <li>
-          <a href="/job-portal/about.php"
+          <a href="./pages/about.php"
             class="block py-2 px-3 text-white rounded hover:bg-gray-500">About</a>
         </li>
         <li>
-          <a href="#"
+          <a href="./pages/services.php"
             class="block py-2 px-3 text-white rounded hover:bg-gray-500">Services</a>
         </li>
         <li>
-          <a href="#"
+          <a href="./pages/contact.php"
             class="block py-2 px-3 text-white rounded hover:bg-gray-500">Contact</a>
         </li>
       </ul>
